f3fc8a7f1199d5b62e825be5056e6d35e02f0758
alanjcuellar <alanjc_@outlook.com>
2021-12-03 19:54:40 -0300

    actividad obligatoria 2

---
+++ AO2/index.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "Bienvenido al sistema <br>";
+echo '<font background-color="red" style="
+background-color: turquoise;
+color: grey;
+">Los datos ingresados son: <br></font>';
+echo '<FONT COLOR="red">';
+echo "Tu usuario: User2020 <br><br>";
+echo "Tu clave: 12345";
+echo '</FONT>';
+
+
+
+
+
+?>
